f5a26fb0e2e6fd30f63f0b480abfd8e268385484
OMARMARCO23 <omarmarco2023@GMAIL.com>
2025-11-20 00:47:16 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Referrer-Policy", value: "no-referrer" },
+          { key: "Permissions-Policy", value: "browsing-topics=()" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: "Cross-Origin-Resource-Policy", value: "same-origin" },
+          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; img-src 'self' blob: data:; style-src 'self' 'unsafe-inline'; script-src 'self'; connect-src 'self' https://generativelanguage.googleapis.com; base-uri 'none'; frame-ancestors 'none';"
+          }
+        ]
+      }
+    ];
+  }
+};
+module.exports = nextConfig;
