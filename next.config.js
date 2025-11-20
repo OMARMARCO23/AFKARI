ad813211ec10028469f58b3ee0f3d230ae720b36
OMARMARCO23 <omarmarco2023@GMAIL.com>
2025-11-20 01:56:03 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-○  (Static)   prerendered as static content
-ƒ  (Dynamic)  server-rendered on demand
-Traced Next.js server files in: 32.002ms
-Created all serverless functions in: 78.549ms
-Collected static files (public/, static/, .next/static): 3.714ms
-Build Completed in /vercel/output [40s]
-Deploying outputs...
-Deployment completed
-Creating build cache...
-Created build cache: 5.700s
-Uploading build cache [74.13 MB]
-Build cache uploaded: 1.180s
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+};
+
+module.exports = nextConfig;
