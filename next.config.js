fd66cad8495f7c8339bb7f39bf8659f0f74f5a1d
OMARMARCO23 <omarmarco2023@GMAIL.com>
2025-11-20 01:36:04 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Referrer-Policy", value: "no-referrer" },
-          { key: "Permissions-Policy", value: "browsing-topics=()" },
-          { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "X-Frame-Options", value: "DENY" },
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          { key: "Cross-Origin-Resource-Policy", value: "same-origin" },
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; img-src 'self' blob: data:; style-src 'self' 'unsafe-inline'; script-src 'self'; connect-src 'self' https://generativelanguage.googleapis.com; base-uri 'none'; frame-ancestors 'none';",
-          },
-        ],
-      },
-    ];
-  },
-};
-module.exports = nextConfig;
+○  (Static)   prerendered as static content
+ƒ  (Dynamic)  server-rendered on demand
+Traced Next.js server files in: 32.002ms
+Created all serverless functions in: 78.549ms
+Collected static files (public/, static/, .next/static): 3.714ms
+Build Completed in /vercel/output [40s]
+Deploying outputs...
+Deployment completed
+Creating build cache...
+Created build cache: 5.700s
+Uploading build cache [74.13 MB]
+Build cache uploaded: 1.180s
